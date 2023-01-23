d7d42ef51932e8e743c1570375c404cabc403bc8
Nikita <kit.lobanov@icloud.com>
2023-01-24 10:56:21 +1300

    Nearly done with part1

--- HomeWorkJanuary.py
+++ HomeWorkJanuary.py
@@ -1,1 +1,6 @@
-print("Hello World")
+import keyboard
+username = input("Welcome to our new messenger! How can I call you?")
+print("Hello, " + username + "!")
+print("Press any button to quit the program")
+ 
+ 
