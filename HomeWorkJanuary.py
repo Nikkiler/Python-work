28e1f5c28e9258af389a6024a4868fe1f0cf2b89
Nikita <kit.lobanov@icloud.com>
2023-01-30 12:29:12 +1300

    Completed task

--- HomeWorkJanuary.py
+++ HomeWorkJanuary.py
@@ -1,6 +1,8 @@
-import readchar
-import keyboard
 username = input("Welcome to our new messenger! How can I call you?")
-print("Hello, " + username + "!")
-print("Press Any Key To Exit")
-k = readchar.readchar()
+answer = input("Hello, " + username + "! Do you want to say something?")
+answer = answer.lower()
+while answer != "no":
+    say = input("What do you want to say? ")
+    print(username + ': ' + say)
+    answer = input("Hello, " + username + "! Do you want to say something?")
+l = input("Press Any Key To Exit")
