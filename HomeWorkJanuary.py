5a268d48bfdd9f5a639c9b89c49021e22f87e90f
Nikita <kit.lobanov@icloud.com>
2023-01-30 12:29:37 +1300

    Completed task

--- HomeWorkJanuary.py
+++ HomeWorkJanuary.py
@@ -5,4 +5,4 @@ while answer != "no":
     say = input("What do you want to say? ")
     print(username + ': ' + say)
     answer = input("Hello, " + username + "! Do you want to say something?")
-l = input("Press Any Key To Exit")
+l = input("Press Enter To Exit")
