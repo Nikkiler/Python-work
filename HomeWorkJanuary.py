1273c51c4eb86d399ad83c9b7717c9c9141be5bd
Nikita <kit.lobanov@icloud.com>
2023-01-30 12:34:57 +1300

    Added main

--- HomeWorkJanuary.py
+++ HomeWorkJanuary.py
@@ -1,8 +1,9 @@
-username = input("Welcome to our new messenger! How can I call you?")
-answer = input("Hello, " + username + "! Do you want to say something?")
-answer = answer.lower()
-while answer != "no":
-    say = input("What do you want to say? ")
-    print(username + ': ' + say)
+def main():
+    username = input("Welcome to our new messenger! How can I call you?")
     answer = input("Hello, " + username + "! Do you want to say something?")
-l = input("Press Enter To Exit")
+    answer = answer.lower()
+    while answer != "no":
+        say = input("What do you want to say? ")
+        print(username + ': ' + say)
+        answer = input("Hello, " + username + "! Do you want to say something?")
+    l = input("Press Enter To Exit")
