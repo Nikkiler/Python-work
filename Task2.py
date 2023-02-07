87a64996168bd72358baedaee04023219753a03f
Nikita <kit.lobanov@icloud.com>
2023-02-07 20:27:42 +1300

    Part 2 complete

--- Task2.py
+++ Task2.py
@@ -1,9 +1,16 @@
 def main():
+    username = set()
     selection = (input("Welcome to the secret chat please choose one of the following menu items by choosing the corosponding menu item and hitting enter 1. Create a user, 2. Login, 3.Connect to the chat, 4. List all registered users, 5. Exit."))
     options = {'1' , '2' , '3', '4', '5'}
     while selection not in options:
         selection = (input("Welcome to the secret chat please choose one of the following menu items by choosing the corosponding menu item and hitting enter 1. Create a user, 2. Login, 3.Connect to the chat, 4. List all registered users, 5. Exit."))
     while selection != '5':
+        if selection == '1':
+            userinput = input("Please, enter the username")
+            while userinput in username:
+                userinput = input("Username already registered, please enter another name")
+        username.add(userinput)
+        print("User " + userinput + " registered")
         exit = input("You selected " + selection + ", press enter to go back")
         selection = (input("Welcome to the secret chat please choose one of the following menu items by choosing the corosponding menu item and hitting enter 1. Create a user, 2. Login, 3.Connect to the chat, 4. List all registered users, 5. Exit."))
     print("Thank you for using our secret chat!")
