92103da282bfa2a57fbfac29dbd3ac6c17852792
Nikita <kit.lobanov@icloud.com>
2023-02-12 14:12:11 +1300

    Common Requirments completed

--- Task2.py
+++ Task2.py
@@ -1,21 +1,50 @@
 def main():
     username = set()
-    selection = (input("Welcome to the secret chat please choose one of the following menu items by choosing the corosponding menu item and hitting enter 1. Create a user, 2. Login, 3.Connect to the chat, 4. List all registered users, 5. Exit."))
+    print("Welcome to the secret chat")
+    print("please choose one of the following menu items")
+    print("by choosing the corosponding menu item and hitting enter")
+    print("1. Create a user")
+    print("2. Login")
+    print("3.Connect to the chat")
+    print("4. List all registered users")
+    print("5. Exit.")
+    selection = (input(""))
     options = {'1' , '2' , '3', '4', '5'}
     while selection not in options:
-        selection = (input("Welcome to the secret chat please choose one of the following menu items by choosing the corosponding menu item and hitting enter 1. Create a user, 2. Login, 3.Connect to the chat, 4. List all registered users, 5. Exit."))
+        print("Welcome to the secret chat")
+        print("please choose one of the following menu items")
+        print("by choosing the corosponding menu item and hitting enter")
+        print("1. Create a user")
+        print("2. Login")
+        print("3.Connect to the chat")
+        print("4. List all registered users")
+        print("5. Exit.")
+        selection = (input(""))
     while selection != '5':
         if selection == '1':
-            userinput = input("Please, enter the username")
-            while userinput in username:
-                userinput = input("Username already registered, please enter another name")
-            username.add(userinput)
-            print("User " + userinput + " registered")
+            option1(username)
         elif selection == '4':
-            for user in username:
-                print(user)
+            option4(username)
         exit = input("You selected " + selection + ", press enter to go back")
-        selection = (input("Welcome to the secret chat please choose one of the following menu items by choosing the corosponding menu item and hitting enter 1. Create a user, 2. Login, 3.Connect to the chat, 4. List all registered users, 5. Exit."))
+        print("Welcome to the secret chat")
+        print("please choose one of the following menu items")
+        print("by choosing the corosponding menu item and hitting enter")
+        print("1. Create a user")
+        print("2. Login")
+        print("3.Connect to the chat")
+        print("4. List all registered users")
+        print("5. Exit.")
+        selection = (input(""))
     print("Thank you for using our secret chat!")
+def option1(username):
+    userinput = input("Please, enter the username")
+    while userinput in username:
+        userinput = input("Username already registered, please enter another name")
+    username.add(userinput)
+    print("User " + userinput + " registered")
+def option4(username):
+    for user in username:
+        print(user)
+
 if __name__ == '__main__':
     main()
