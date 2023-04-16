dd1fb47d9304b162cb1996c10d14706d11faaaf3
Nikita <kit.lobanov@icloud.com>
2023-04-17 11:19:23 +1200

    Testing

--- text.py
+++ text.py
@@ -1,8 +1,16 @@
 #file obkects
-
+"""""
 with open('test.txt', 'r') as f:
     for line in f:
         print(line, end='')
     f_contents = f.readline()
     print(f_contents)
-
+"""
+import csv
+with open("test.csv", "r") as handler:
+    reader = csv.DictReader(handler, delimiter=',')
+    report = {}
+    for row in reader:
+        print(row)
+        report = report | row
+print(report)
