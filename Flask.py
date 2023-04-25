c1d3e1c09f28e3e1a7d7e2c0b98487df65a5bbf9
Nikita <kit.lobanov@icloud.com>
2023-04-25 17:07:55 +1200

    Working on flask

--- Flask.py
+++ Flask.py
@@ -5,13 +5,27 @@ sio = socketio.Server()
 app = socketio.WSGIApp(sio, static_files={
     '/': {'content_type': 'text/html', 'filename': 'index.html'}
 })
+
+users = {}
+
 @sio.event
-def connect(sid, environ):
+def connect(sid, environ, data):
     print('connect', sid)
 
 @sio.event
 def disconnect(sid):
     print('disconnect ', sid)
+
+@sio.event
+def login(sid, data):
+    print(f"Login event {sid} {data}")
+# @sio.event
+# def message(message):
+#     print(f"Message event {message}")
+@sio.event
+def register(sid, username):
+    users[sid] = username
+    sio.emit('messanger', sid, username)
 
 if __name__ == '__main__':
     eventlet.wsgi.server(eventlet.listen(('', 5001)), app)
--- Flaskclient.py
+++ Flaskclient.py
@@ -1,15 +1,32 @@
 import socketio
 
 sio = socketio.Client()
+
 @sio.event
 def connect():
     print('connection established')
+    print('1.Login')
+    print('2.Send message')
+    choice = input('')
+    while choice != '1' and choice != '2':
+        print('Please enter either 1 or 2')
+    if choice == '1':
+        print("Enter username")
+        username = input('')
+        print('User registered')
+        sio.emit('register', username)
+    else:
+        print("What would you like to send")
+        message = input('')
+        sio.emit('message_r', message)
+
 
 @sio.event
 def disconnect():
     print('disconnected from server')
-
-
+@sio.event
+def messanger(sid, username):
+    print(f"Welcome {sid} : {username}")
 
 sio.connect('http://localhost:5001')
 sio.wait()
